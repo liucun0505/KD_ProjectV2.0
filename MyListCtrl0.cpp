225398bd559b3e9922cffc996330ab7444ef0fb0
liucun <liucun0505@126.com>
2018-01-11 16:53:28 +0800

    1
    1

---
+++ MyListCtrl0.cpp
@@ -0,0 +1,30 @@
+// MyListCtrl0.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "KD_Project.h"
+#include "MyListCtrl0.h"
+
+
+// CMyListCtrl0
+
+IMPLEMENT_DYNAMIC(CMyListCtrl0, CListCtrl)
+
+CMyListCtrl0::CMyListCtrl0()
+{
+
+}
+
+CMyListCtrl0::~CMyListCtrl0()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CMyListCtrl0, CListCtrl)
+END_MESSAGE_MAP()
+
+
+
+// CMyListCtrl0 消息处理程序
+
+
---
+++ MyListCtrl0.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+// CMyListCtrl0
+
+class CMyListCtrl0 : public CListCtrl
+{
+	DECLARE_DYNAMIC(CMyListCtrl0)
+
+public:
+	CMyListCtrl0();
+	virtual ~CMyListCtrl0();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
