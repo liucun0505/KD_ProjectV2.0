4d76b170d8d5af7728d1dc143ade0a3b2021bdd7
liucun <liucun0505@126.com>
2018-01-12 09:43:36 +0800

    加了 MyListCtrl_1类
    加了 MyListCtrl_1类

---
+++ MyListCtrl_1.cpp
@@ -0,0 +1,30 @@
+// MyListCtrl_1.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "KD_Project.h"
+#include "MyListCtrl_1.h"
+
+
+// MyListCtrl_1
+
+IMPLEMENT_DYNAMIC(MyListCtrl_1, CListCtrl)
+
+MyListCtrl_1::MyListCtrl_1()
+{
+
+}
+
+MyListCtrl_1::~MyListCtrl_1()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(MyListCtrl_1, CListCtrl)
+END_MESSAGE_MAP()
+
+
+
+// MyListCtrl_1 消息处理程序
+
+
---
+++ MyListCtrl_1.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+// MyListCtrl_1
+
+class MyListCtrl_1 : public CListCtrl
+{
+	DECLARE_DYNAMIC(MyListCtrl_1)
+
+public:
+	MyListCtrl_1();
+	virtual ~MyListCtrl_1();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
